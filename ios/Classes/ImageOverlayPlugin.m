54f92660f0bd7f1c220b1d9facda64946e8a24b0
Evgeny Vasilyev <zeejfps@Evgenys-iMac.local>
2019-08-09 09:37:03 -0500

    Added IOS version of the plugin

--- ios/Classes/ImageOverlayPlugin.m
+++ ios/Classes/ImageOverlayPlugin.m
@@ -2,19 +2,39 @@
 
 @implementation ImageOverlayPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"image_overlay"
-            binaryMessenger:[registrar messenger]];
-  ImageOverlayPlugin* instance = [[ImageOverlayPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"image_overlay"
+                                     binaryMessenger:[registrar messenger]];
+    ImageOverlayPlugin* instance = [[ImageOverlayPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"overlayImages" isEqualToString:call.method]) {
+        NSString *srcPath = call.arguments[@"src"];
+        NSString *dstPath = call.arguments[@"dst"];
+        UIImage *srcImg = [UIImage imageWithContentsOfFile:srcPath];
+        UIImage *dstImg = [UIImage imageWithContentsOfFile:dstPath];
+        
+        CGFloat aspect = srcImg.size.width / srcImg.size.height;
+        
+        CGFloat width = dstImg.size.width * dstImg.scale;
+        CGFloat height = width / aspect;
+        
+        UIGraphicsBeginImageContextWithOptions(dstImg.size, NO, 1.0);
+        
+        [dstImg drawAtPoint:CGPointZero];
+        [srcImg drawInRect:CGRectMake(0, dstImg.size.height - height, width, height)];
+        
+        UIImage *endImg = UIGraphicsGetImageFromCurrentImageContext();
+        NSData *endImgData = UIImageJPEGRepresentation(endImg, 80);
+        
+        [endImgData writeToFile:dstPath atomically:YES];
+        
+        result(@"Success!");
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
